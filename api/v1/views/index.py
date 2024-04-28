279175df91db7144b8ccf8c09d46bf0b212199d6
talk2dennis <adigwedennis@yahoo.com>
2024-04-28 15:18:29 +0100

    fixed /stats routenow returns all the queries of all classes rightly

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,12 @@
 """
 THis is a python file
 """
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -16,11 +22,11 @@ def status():
 def stats():
     """ returns the status of the data in the database """
     response = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("Users")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(response)
