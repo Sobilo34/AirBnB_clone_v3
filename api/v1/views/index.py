3bb3cc3c4688ac7cb12d71105684494287e5859f
talk2dennis <adigwedennis@yahoo.com>
2024-04-28 15:09:15 +0100

    fixed /stats route to return all the queries of all class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,23 @@ THis is a python file
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
     """The status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """ returns the status of the data in the database """
+    response = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("Users")
+    }
+    return jsonify(response)
