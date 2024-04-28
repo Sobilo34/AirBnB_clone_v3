ddd27b21f1405e2744c4e47f5e8ec960ea6de73e
talk2dennis <adigwedennis@yahoo.com>
2024-04-28 14:56:29 +0100

    fixed coding style

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,11 +11,14 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """ Closes database"""
     storage.close()
 
 
-if __name__== "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=getenv("HBNB_API_PORT", 5000), threaded=True, debug=True)
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host=host, port=port, threaded=True, debug=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ THis is a python file
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route("/status")
 def status():
     """The status"""
