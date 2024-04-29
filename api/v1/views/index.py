1c620d7ccaf95c2eb357bb39fb576917026bbca8
Sobilo34 <bilalsolih60@gmail.com>
2024-04-29 11:52:51 +0100

    try another way

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,13 +16,11 @@ def status():
 @app_views.route('/api/v1/stats', strict_slashes=False)
 def stats():
     """ returns the status of the data in the database """
-    response = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
+    class_list = [Amenity, City, State, Place, Review, User]
+    names = ["amenities", "cities", "states", "places", "reviews", "users"]
+    response = {}
+    
+    for name, cls in zip(names, class_list):
+            response[name] = storage.count(cls=cls)
     return jsonify(response), 200
    
