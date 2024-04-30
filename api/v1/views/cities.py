5ec46d0c13b1afb9313bec9742beac84153683a6
talk2dennis <adigwedennis@yahoo.com>
2024-04-30 14:12:00 +0100

    returned the list of cities of a state: fixed import error

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,6 +5,7 @@ Creates states route
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
+from models.state import State
 from models.city import City
 
 
