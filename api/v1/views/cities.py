929577313060168da61ebc83b389f5edff340675
talk2dennis <adigwedennis@yahoo.com>
2024-04-30 13:59:54 +0100

    returned the list of cities of a state

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,3 +7,14 @@ from flask import jsonify, abort, request, make_response
 from models import storage
 from models.city import City
 
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+def get_cities(state_id):
+    """ returns list of all cities of a state """
+    state = storage.get(State, state_id)
+    # return 404 if state not found
+    if not state:
+        abort(404)
+    # returns the list of cities of a state if the state exists
+    city_list = [city.to_dict() for city in state.cities]
+    return jsonify(city_list)
