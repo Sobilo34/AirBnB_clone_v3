383c5b041ba56a7021f5bd9f3d3c30ad28dac38f
talk2dennis <adigwedennis@yahoo.com>
2024-04-30 12:21:50 +0100

    updated index.py the stats method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@ THis is a python file
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -16,12 +22,13 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ returns the status of the data in the database """
-    _response = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+    counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
-    return jsonify(_response), 200
+
+    return jsonify(counts)
