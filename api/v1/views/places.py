e9d5aa935f1d45e3b5405de06b6971a53f9213b6
talk2dennis <adigwedennis@yahoo.com>
2024-05-01 05:20:41 +0100

    started place route

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,40 +5,40 @@ Creates cities route
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
-from models.state import State
+from models.place import Place
 from models.city import City
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'],
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
-def get_cities(state_id):
-    """ returns list of all cities of a state """
-    state = storage.get(State, state_id)
+def get_places(city_id):
+    """ returns list of all places in a city """
+    city = storage.get(City, city_id)
     # return 404 if state not found
-    if not state:
+    if not city:
         abort(404)
     # returns the list of cities of a state if the state exists
-    city_list = [city.to_dict() for city in state.cities]
-    return jsonify(city_list)
+    place_list = [place.to_dict() for place in city.places]
+    return jsonify(place_list)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
-def get_city(city_id):
-    """ returns list of all cities of a state """
-    city = storage.get(City, city_id)
-    # return 404 if state not found
-    if not city:
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def get_place(place_id):
+    """ returns list of a place in a city """
+    place = storage.get(Place, place_id)
+    # return 404 if place not found
+    if not place:
         abort(404)
-    return jsonify(city.to_dict())
+    return jsonify(place.to_dict())
 
 
-@app_views.route("/cities/<city_id>", methods=['DELETE'],
+@app_views.route("/places/<place_id>", methods=['DELETE'],
                  strict_slashes=False)
-def delete_city(city_id):
+def delete_city(place_id):
     """ return a state with a given id """
-    city = storage.get(City, city_id)
-    if not city:
+    place = storage.get(Place, place_id)
+    if not place:
         abort(404)
-    storage.delete(city)
+    storage.delete(place)
     storage.save()
     return make_response(jsonify({}), 200)
