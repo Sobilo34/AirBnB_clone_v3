b0c713d9e421ff406fc2a06821eecaa447bc22c7
talk2dennis <adigwedennis@yahoo.com>
2024-05-01 04:25:52 +0100

    made files executed

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -42,39 +42,3 @@ def delete_city(city_id):
     storage.delete(city)
     storage.save()
     return make_response(jsonify({}), 200)
-
-
-@app_views.route('/states/<state_id>/cities', methods=['POST'],
-                 strict_slashes=False)
-def post_city(state_id):
-    """ creates a city """
-    state = storage.get(State, state_id)
-    # return 404 if state not found
-    if not state:
-        abort(404)
-    if not request.is_json:
-        abort(400, description='Not a JSON')
-    if 'name' not in request.get_json():
-        abort(400, description='Missing name')
-    data = request.get_json()
-    city = City(**data)
-    city.state_id = state.id
-    city.save()
-    return make_response(jsonify(city.to_dict()), 201)
-
-
-@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-def update_city(city_id):
-    """ updates the city instance """
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-    if not request.is_json:
-        abort(400, description="Not a JSON")
-    data = request.get_json()
-    ignore = ["id", "created_at", "updated_at", "state_id"]
-    for key, value in data.items():
-        if key not in ignore:
-            setattr(city, key, value)
-    storage.save()
-    return make_response(jsonify(city.to_dict()), 200)
