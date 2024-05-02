b2d019c8f22cade2af06cefa84f4f185a5ee1431
Bilal Oyeleke <122975292+Sobilo34@users.noreply.github.com>
2024-05-02 23:23:43 +0100

    revert back for try

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,63 +1,70 @@
-#!/usr/bin/python3
-"""State objects that handles all default RESTFul API actions"""
 
+#!/usr/bin/python3
+"""
+Creates states route
+"""
 from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
-from flask import abort, request, jsonify
 
 
-@app_views.route("/states", strict_slashes=False, methods=["GET"])
-@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"])
-def states(state_id=None):
-    """show states and states with id"""
-    states_list = []
-    if state_id is None:
-        all_objs = storage.all(State).values()
-        for v in all_objs:
-            states_list.append(v.to_dict())
-        return jsonify(states_list)
-    else:
-        result = storage.get(State, state_id)
-        if result is None:
-            abort(404)
-        return jsonify(result.to_dict())
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """ returns list of all state """
+    states = storage.all(State)
+    state_list = [state.to_dict() for state in states.values()]
+    return jsonify(state_list)
 
 
-@app_views.route("/states/<state_id>", strict_slashes=False,
-                 methods=["DELETE"])
-def states_delete(state_id):
-    """delete method"""
-    obj = storage.get(State, state_id)
-    if obj is None:
+@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ return a state with a given id """
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    storage.delete(obj)
+    return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>", methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """ return a state with a given id """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route("/states", strict_slashes=False, methods=["POST"])
-def create_state():
-    """create a new post req"""
-    data = request.get_json(force=True, silent=True)
-    if not data:
-        abort(400, "Not a JSON")
-    if "name" not in data:
-        abort(400, "Missing name")
-    new_state = State(**data)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """ creates a state """
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    if "name" not in request.get_json():
+        abort(400, description="Missing name")
+    data = request.get_json()
+    state = State(**data)
+    storage.new(state)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 201)
 
 
-@app_views.route("/states/<state_id>", strict_slashes=False, methods=["PUT"])
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """update state"""
-    obj = storage.get(State, state_id)
-    if obj is None:
+    """ updates the state instance """
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    data = request.get_json(force=True, silent=True)
-    if not data:
-        abort(400, "Not a JSON")
-    obj.name = data.get("name", obj.name)
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    # return if ignore member is present in the json
+    data = request.get_json()
+    ignore = ["id", "created_at", "updated_at"]
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
