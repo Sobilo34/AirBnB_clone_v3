835dcbea7f0b72ea4da074b8ee8ba4e89e26269d
Sobilo34 <bilalsolih60@gmail.com>
2024-05-03 00:21:58 +0100

    updated __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,7 @@ This is to initialize the packages
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *  # nopep8
 from api.v1.views.states import *  # nopep8
 from api.v1.views.amenities import *  # nopep8
@@ -12,3 +13,4 @@ from api.v1.views.cities import *  # nopep8
 from api.v1.views.users import *  # nopep8
 from api.v1.views.places_reviews import *  # nopep8
 from api.v1.views.places import *  # nopep8
+from api.v1.views.places_amenities import  # nopep8
