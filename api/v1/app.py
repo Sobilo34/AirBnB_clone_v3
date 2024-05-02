4c99a9003d0bfa03cd70e4200e33225001f396ff
Bilal Oyeleke <122975292+Sobilo34@users.noreply.github.com>
2024-05-02 10:10:17 +0100

    Update app.py: Added the CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-An app
+An API application
 """
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
-app.register_blueprint(app_views)
-
+app.url_map.strict_slashes = False
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -20,10 +22,14 @@ def teardown(exception):
 
 @app.errorhandler(404)
 def not_found(error):
+    """ Handle error"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
+    """
+    set the defaults
+    """
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True, debug=True)
